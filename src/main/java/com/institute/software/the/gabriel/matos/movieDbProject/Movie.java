dd9a415270232b3f595f47a2a52e477d8cfadf73
Gabriel <gab.matos@outlook.com>
2021-11-03 20:20:51 +0000

    Added additional attributes, getters, setters and respective tests

--- src/main/java/com/institute/software/the/gabriel/matos/movieDbProject/Movie.java
+++ src/main/java/com/institute/software/the/gabriel/matos/movieDbProject/Movie.java
@@ -7,38 +7,82 @@ public class Movie {
 
     @Id
     @Column(name = "film_id")
-    private int id;
-
-
-
+    private int film_id;
     private String title;
-
+    private String description;
+    private int release_year;
+    private int language_id;
+    private int duration;
     private String rating;
 
-    public Movie(String title, String rating) {
+    public Movie(int film_id, String title, String description, int release_year, int language_id, int duration, String rating) {
+        this.film_id= film_id;
         this.title = title;
+        this.description = description;
+        this.release_year = release_year;
+        this.language_id = language_id;
+        this.duration = duration;
         this.rating = rating;
     }
     public Movie(){
 
     }
 
-    public String getTitle() {
+    public int getFilm_id() {
+        return film_id;
+    }
 
+    public void setId(int id) {
+        this.film_id = id;
+    }
+
+    public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-
+    public void  setTitle(String title) {
         this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void  setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRelease_year() {
+        return release_year;
+    }
+
+    public void  setRelease_year(int release_year) {
+        this.release_year = release_year;
+    }
+
+    public int getLanguage_id() {
+        return language_id;
+    }
+
+    public void  setLanguage_id(int language) {
+        this.language_id = language;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void  setDuration(int duration) {
+        this.duration = duration;
     }
 
     public String getRating() {
         return rating;
     }
 
-    public void setRating(String rating) {
-
+    public void  setRating(String rating) {
         this.rating = rating;
     }
+
+
 }
--- src/test/java/com/institute/software/the/gabriel/matos/movieDbProject/MovieTest.java
+++ src/test/java/com/institute/software/the/gabriel/matos/movieDbProject/MovieTest.java
@@ -6,42 +6,106 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class MoviesDbApplicationTests {
+class MovieTest {
 
 	@Test
 	void contextLoads() {
 	}
-	@Test
-	public void testMoviegetTitle(){
 
-		Movie testFilm = new Movie ("The Hunger Games", "4");
-		assertEquals("The Hunger Games", testFilm.getTitle());
-	}
-	@Test
-	public void testMoviegetRating(){
+	//Testing getters
 
-		Movie testFilm = new Movie ("The Hunger Games", "4");
-		assertEquals("4",testFilm.getRating());
+	@Test
+	public void testFilmId() {
+		Movie movieTest = new Movie(999, "The Hunger Games", "Bows and guns", 2013, 1, 121, "PG-13");
+		assertEquals(999, movieTest.getFilm_id(), "Wrong film id");
 	}
 
+	@Test
+	public void testMovieTitle(){
+		Movie movieTest = new Movie (999, "The Hunger Games","Bows and guns",2013,1,121,"PG-13");
+		assertEquals("The Hunger Games", movieTest.getTitle(), "Wrong movie title");
+	}
 
-//	@Test
-//	public void testMoviesetTitle(){
-//
-//		Movie testFilm = new Movie();
-//		testFilm.setTitle("Harry Potter");
-//		assertEquals("The title was incorrect", "Harry Potter", testFilm.getTitle());
-//	}
-//	@Test
-//	public void testMoviesetRating(){
-//
-//		Movie testFilm = new Movie();
-//		testfilm.setRating("R");
-//		assertEquals("The rating was incorrect", "R", testfilm.getRating());
-//	}
+	@Test
+	public void testYearOfRelease() {
+		Movie movieTest = new Movie(999, "The Hunger Games", "Bows and guns", 2013, 1, 121, "PG-13");
+		assertEquals(2013, movieTest.getRelease_year(), "Wrong release year");
+	}
 
-//
+	@Test
+	public void durationOfMovie() {
+		Movie movieTest = new Movie(999, "The Hunger Games", "Bows and guns", 2013, 1, 121, "PG-13");
+		assertEquals(999, movieTest.getFilm_id(), "Wrong valid ID");
+	}
 
+	@Test
+	public void ratingOfMovie() {
+		Movie movieTest = new Movie(999, "The Hunger Games", "Bows and guns", 2013, 1, 121, "PG-13");
+		assertEquals("PG-13", movieTest.getRating(), "Wrong movie duration");
+	}
+
+	@Test
+	public void testLanguageId() {
+		Movie movieTest = new Movie(999, "The Hunger Games", "Bows and guns", 2013, 1, 121, "PG-13");
+		assertEquals(1, movieTest.getLanguage_id(), "Wrong language ID");
+	}
+
+	@Test
+	public void testDescription() {
+		Movie movieTest = new Movie(999, "The Hunger Games", "Bows and guns", 2013, 1, 121, "PG-13");
+		assertEquals("Bows and guns", movieTest.getDescription(), "Wrong description");
+	}
+
+	//Testing Setters
+
+	@Test
+	public void testSetFilmID() {
+		Movie movieTest = new Movie();
+		movieTest.setId(999);
+		assertEquals(999, movieTest.getFilm_id(), "The film id was incorrect");
+	}
+
+	@Test
+	public void testSetTitleMethod() {
+		Movie movieTest = new Movie();
+		movieTest.setTitle("The Hunger Games");
+		assertEquals("The Hunger Games", movieTest.getTitle(), "The title was incorrect");
+	}
+
+	@Test
+	public void testSetDescription() {
+		Movie movieTest = new Movie();
+		movieTest.setDescription("Bows and guns");
+		assertEquals("Bows and guns", movieTest.getDescription(), "The description is incorrect");
+	}
+
+	@Test
+	public void testSetRelease_Year() {
+		Movie movieTest = new Movie();
+		movieTest.setRelease_year(2013);
+		assertEquals(2013, movieTest.getRelease_year(), "The release year was incorrect");
+	}
+
+	@Test
+	public void testSetLanguage() {
+		Movie movieTest = new Movie();
+		movieTest.setLanguage_id(1);
+		assertEquals(1, movieTest.getLanguage_id(), "The language was incorrect");
+	}
+
+	@Test
+	public void testSetDuration() {
+		Movie movieTest = new Movie();
+		movieTest.setDuration(121);
+		assertEquals(121, movieTest.getDuration(), "The duration was incorrect");
+	}
+
+	@Test
+	public void testSetRating() {
+		Movie movieTest = new Movie();
+		movieTest.setRating("PG-13");
+		assertEquals("PG-13", movieTest.getRating(), "The rating was incorrect");
+	}
 
 }
 
